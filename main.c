c9de9850e1c2cc2e7917d36162b4e64021996681
MiguelCigano <miguel_m0130@hotmail.com>
2024-08-16 18:50:29 -0700

    Solo comence otro proyecto

--- main.c
+++ main.c
@@ -9,26 +9,15 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "Probando Raylib");
 
-    int posicionY = screenHeight/2 - 40;
-    int velocidadScroll = 8;            
-
-    SetTargetFPS(60);         
+    Color screen_buffer_data     
  
     while (!WindowShouldClose())    
     {
 
-        posicionY -= (int)(GetMouseWheelMove()*velocidadScroll);
- 
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
-
-            DrawRectangle(screenWidth/2 - 40, posicionY, 80, 80, BLUE);
-
-            DrawText("Puedes mover la caja", 10, 10, 20, BLACK);
-            DrawText(TextFormat("Posicion de la caja en Y: %03i", posicionY), 10, 40, 20, MAGENTA);
-
-        EndDrawing();  
+        EndDrawing();
+ 
     }
 
     CloseWindow();        
